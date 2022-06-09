62aaed4355a0e314b813ecae57a5e4b1d6b4e5f3
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-06-09 12:06:17 +0200

    first commit

---
+++ components/Screen.js
@@ -0,0 +1,12 @@
+import { Textfit } from "react-textfit";
+import styles from"./Screen.module.css";
+
+const Screen = ({ value }) => {
+  return (
+    <Textfit className={styles.screen} mode="single" max={70}>
+      {value}
+    </Textfit>
+  );
+};
+
+export default Screen;
